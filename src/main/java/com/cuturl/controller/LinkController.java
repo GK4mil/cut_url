146925f6e323c0cdd775bccdeec8eb4742a4183c
GK4mil <kamilg62@o2.pl>
2020-04-25 00:39:44 +0200

    test

--- src/main/java/com/cuturl/controller/LinkController.java
+++ src/main/java/com/cuturl/controller/LinkController.java
@@ -1,24 +1,15 @@
 package com.cuturl.controller;
 
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.http.HttpHeaders;
-
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.view.RedirectView;
 
 import com.cuturl.model.Link;
 import com.cuturl.service.LinkService;
-
 
 @Controller
 public class LinkController {
@@ -28,6 +19,8 @@ public class LinkController {
 	@PostMapping("/generate")
 	String generate(@ModelAttribute Link link, Model model) {
 		System.out.println("generating");
+		if (!(link.getLongURL().contains("https://") || link.getLongURL().contains("http://")))
+			link.setLongURL("http://" + link.getLongURL());
 		linkService.addLink(link, 5);
 		model.addAttribute("tinyURL", "localhost:8080/c/" + link.getCode());
 		return "result";
@@ -38,22 +31,25 @@ public class LinkController {
 		model.addAttribute("link", new Link());
 		return "index";
 	}
-	
 
 	@GetMapping("/c/{code}")
-	String code(Model model,@PathVariable String code, HttpServletResponse httpServletResponse) {
+	String code(Model model, @PathVariable String code) {
 		System.out.println(code.length());
 		if (code.length() > 0) {
 			System.out.println("code exists");
-			return "redirect: /redirect";
+			return "redirect:" + linkService.getLink(code);
 		} else {
 			System.out.println("no code");
 			model.addAttribute("link", new Link());
 			return "redirect:/";
 		}
 	}
-	
-	
+
+//	@RequestMapping(value="/{url}", method = RequestMethod.GET)
+//	public ModelAndView method(#PathVariable("url") String url) {
+//	String original = database.getLongUrl(UrlShortener.decode(url));
+//	return new ModelAndView("redirect:" + original);
+	// }
 //	@GetMapping("/test")
 //	String test(Model model) {
 //		model.addAttribute("code",linkService.codeGeneration(5));
